2ab7aa80e07c8f7e65f3107280ac6239de1784dc
Michael van de Ven <michael.vd.ven95@gmail.com>
2016-08-15 15:13:21 +0200

    first draw open en close file

---
+++ logic board/logic board/Application.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace logic_board
+{
+    partial class Application
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.buttonRunFile = new System.Windows.Forms.Button();
+            this.textBoxBoardFormat = new System.Windows.Forms.TextBox();
+            this.labelFileName = new System.Windows.Forms.Label();
+            this.buttonSaveFile = new System.Windows.Forms.Button();
+            this.buttonOpenFile = new System.Windows.Forms.Button();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.splitContainer1.Panel1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.buttonRunFile);
+            this.splitContainer1.Panel2.Controls.Add(this.textBoxBoardFormat);
+            this.splitContainer1.Panel2.Controls.Add(this.labelFileName);
+            this.splitContainer1.Panel2.Controls.Add(this.buttonSaveFile);
+            this.splitContainer1.Panel2.Controls.Add(this.buttonOpenFile);
+            this.splitContainer1.Panel2.Margin = new System.Windows.Forms.Padding(10);
+            this.splitContainer1.Panel2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.splitContainer1.Panel2MinSize = 200;
+            this.splitContainer1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.splitContainer1.Size = new System.Drawing.Size(1184, 661);
+            this.splitContainer1.SplitterDistance = 456;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // buttonRunFile
+            // 
+            this.buttonRunFile.Location = new System.Drawing.Point(477, 97);
+            this.buttonRunFile.Name = "buttonRunFile";
+            this.buttonRunFile.Size = new System.Drawing.Size(75, 23);
+            this.buttonRunFile.TabIndex = 5;
+            this.buttonRunFile.Text = "Run File";
+            this.buttonRunFile.UseVisualStyleBackColor = true;
+            // 
+            // textBoxBoardFormat
+            // 
+            this.textBoxBoardFormat.Location = new System.Drawing.Point(3, 3);
+            this.textBoxBoardFormat.Multiline = true;
+            this.textBoxBoardFormat.Name = "textBoxBoardFormat";
+            this.textBoxBoardFormat.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxBoardFormat.Size = new System.Drawing.Size(450, 193);
+            this.textBoxBoardFormat.TabIndex = 4;
+            // 
+            // labelFileName
+            // 
+            this.labelFileName.AutoSize = true;
+            this.labelFileName.Cursor = System.Windows.Forms.Cursors.Default;
+            this.labelFileName.Location = new System.Drawing.Point(459, 6);
+            this.labelFileName.MaximumSize = new System.Drawing.Size(250, 0);
+            this.labelFileName.MinimumSize = new System.Drawing.Size(250, 0);
+            this.labelFileName.Name = "labelFileName";
+            this.labelFileName.Size = new System.Drawing.Size(250, 13);
+            this.labelFileName.TabIndex = 2;
+            this.labelFileName.Text = "Geen bestand geselecteerd";
+            this.labelFileName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // buttonSaveFile
+            // 
+            this.buttonSaveFile.Location = new System.Drawing.Point(477, 68);
+            this.buttonSaveFile.Name = "buttonSaveFile";
+            this.buttonSaveFile.Size = new System.Drawing.Size(75, 23);
+            this.buttonSaveFile.TabIndex = 1;
+            this.buttonSaveFile.Text = "Save File";
+            this.buttonSaveFile.UseVisualStyleBackColor = true;
+            this.buttonSaveFile.Click += new System.EventHandler(this.buttonSaveFile_Click);
+            // 
+            // buttonOpenFile
+            // 
+            this.buttonOpenFile.Location = new System.Drawing.Point(477, 39);
+            this.buttonOpenFile.Name = "buttonOpenFile";
+            this.buttonOpenFile.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpenFile.TabIndex = 0;
+            this.buttonOpenFile.Text = "Open File";
+            this.buttonOpenFile.UseVisualStyleBackColor = true;
+            this.buttonOpenFile.Click += new System.EventHandler(this.buttonOpenFile_Click);
+            // 
+            // Application
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 661);
+            this.Controls.Add(this.splitContainer1);
+            this.MaximumSize = new System.Drawing.Size(1920, 1080);
+            this.MinimumSize = new System.Drawing.Size(1200, 700);
+            this.Name = "Application";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Logic board";
+            this.Load += new System.EventHandler(this.Application_Load);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button buttonOpenFile;
+        private System.Windows.Forms.Label labelFileName;
+        private System.Windows.Forms.Button buttonSaveFile;
+        private System.Windows.Forms.TextBox textBoxBoardFormat;
+        private System.Windows.Forms.Button buttonRunFile;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
+
---
+++ logic board/logic board/Application.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace logic_board
+{
+    public partial class Application : Form
+    {
+        public Application()
+        {
+            InitializeComponent();
+        }
+
+        private void Application_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonOpenFile_Click(object sender, EventArgs e)
+        {
+
+            openFileDialog.Title = "Open een logisch circuit";
+            openFileDialog.Filter = "Text Files (.txt)|*.txt|Circuits (.cir)|*.cir";
+
+            try {
+                if (openFileDialog.ShowDialog() == DialogResult.OK) {
+
+                    StreamReader readedFile = new StreamReader(File.OpenRead(openFileDialog.FileName));
+
+                    labelFileName.Text = openFileDialog.SafeFileName;
+                    textBoxBoardFormat.Text = readedFile.ReadToEnd();
+
+                    readedFile.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Opening File", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void buttonSaveFile_Click(object sender, EventArgs e)
+        {
+
+            openFileDialog.Title = "Save een logisch circuit";
+            openFileDialog.Filter = "Text Files (.txt)|*.txt|Circuits (.cir)|*.cir";
+
+            try
+            {
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    StreamWriter sw = new StreamWriter(File.Create(saveFileDialog.FileName));
+                    sw.Write(textBoxBoardFormat.Text);
+                    sw.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Saving File", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
